a432dd4c312642ad9094eef50a5f9798112ba9ac
dalmaszekely <dalmaszekely@users.noreply.github.com>
2020-12-12 21:47:02 +0100

    design

---
+++ cluster.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    var title = document.createElement("P");
+    title.innerHTML = "Relevant financial products";
+    title.id = "title";
+    document.getElementById("inner").appendChild(title);
+
+});
--- pux.js
+++ pux.js
@@ -13,18 +13,18 @@ var time_for_passion;
 var cluster = 0;
 
 
-function register(){
+function register() {
     getInputs();
 }
 
-function getInputs(){
+function getInputs() {
 
     getBMI();
     if (BMI_range == 2) cluster += 1;
     else if (BMI_range == 1) cluster += 2;
 
     if (document.getElementById("sufficient_income").checked)
-    sufficient_income = 1;
+        sufficient_income = 1;
     else sufficient_income = 0;
 
     if (sufficient_income == 1) cluster += 3;
@@ -50,20 +50,20 @@ function getInputs(){
     daily_shouting = document.getElementById("daily_shouting").value;
     if (daily_shouting <= 2) cluster += 2;
     else if (daily_shouting == 3) cluster += 3;
-    else if (daily_shouting >=4) cluster += 1;
+    else if (daily_shouting >= 4) cluster += 1;
 
     supporting_others = document.getElementById("supporting_others").value;
     if (supporting_others <= 3) cluster += 1;
     else if (supporting_others <= 5) cluster += 2;
     else if (supporting_others <= 7) cluster += 4;
     else cluster += 3;
-    
+
     achievement = document.getElementById("achievement").value;
     if (achievement <= 2) cluster += 1;
     else if (achievement <= 3) cluster += 2;
     else if (achievement <= 5) cluster += 4;
     else cluster += 3;
-    
+
     live_vision = document.getElementById("live_vision").value;
     if (live_vision <= 2) cluster += 1;
     else if (live_vision == 3) cluster += 2;
@@ -86,15 +86,15 @@ function getInputs(){
 
 }
 
-function getBMI(){
+function getBMI() {
     var weight = document.getElementById("weight").value;
     var height = document.getElementById("height").value / 100;
-    var BMI = weight / (height*height);
-    if(BMI > 25)
+    var BMI = weight / (height * height);
+    if (BMI > 25)
         BMI_range = 2;
     else BMI_range = 1;
 }
 
-function openPersonalPage(cluster){
-    location.replace("./cluster_" + cluster + ".html")
+function openPersonalPage(cluster) {
+    location.replace("./cluster_" + cluster + ".html");
 }
